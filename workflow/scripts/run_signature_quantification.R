024179c890ca410e2d395825a7c331778351de9c
Phil9S <philip.smith@cruk.cam.ac.uk>
2023-07-20 14:53:39 +0100

    export sig data

--- workflow/scripts/run_signature_quantification.R
+++ workflow/scripts/run_signature_quantification.R
@@ -19,3 +19,15 @@ quant.sigs <- quantifyCNSignatures(object=INPUT,
     build=GENOME)
 
 saveRDS(object=quant.sigs,file=OUTPUT)
+
+OUTDIR <- dirname(OUTPUT)
+write.table(x = getActivities(quant.sigs),
+	file = paste0(OUTDIR,"cn_sigs_activites.tsv"),
+	append = FALSE, quote = FALSE,row.names = TRUE,
+ 	col.names = TRUE, sep = "\t")
+
+write.table(x = getSampleByComponent(quant.sigs),
+        file = paste0(OUTDIR,"cn_sigs_SxC.tsv"),
+        append = FALSE, quote = FALSE,row.names = TRUE,
+        col.names = TRUE, sep = "\t")
+
